9c470cc9941f80014658ccbdc93fc4c87d79b5a2
add <147607841+AnmolrPunj@users.noreply.github.com>
2026-09-13 10:03:11 +1000

    expansion of data + details

--- index.js
+++ index.js
@@ -214,9 +214,10 @@ function isCitySuppressed(c, t) {
 }
 
 function renderTooltip(b) {
+    const summaryHtml = `<ul id="tt-summary">${b.summary.map(s => `<li>${s}</li>`).join("")}</ul>`;
     tooltip
         .style("display", "block")
-        .html(`<span id="tt-close">×</span><h3>${b.name}</h3><div class="dates">${b.dates}</div><div id="tt-summary">${b.summary}</div><a id="tt-readmore" href="info/index.html?id=${b.id}">Read more →</a>`);
+        .html(`<span id="tt-close">×</span><h3>${b.name}</h3><div class="dates">${b.dates}</div>${summaryHtml}<a id="tt-readmore" href="info/index.html?id=${b.id}">Read more →</a>`);
 }
 
 function computeFinalTransform(b) {
